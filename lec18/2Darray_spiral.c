b87423175594f7b27cf96600675c437118c21745
Jishaan Khatri <jishaan123465@gmail.com>
2025-09-24 22:10:59 +0530

    spiral

--- lec18/2Darray_spiral.c
+++ lec18/2Darray_spiral.c
@@ -3,30 +3,28 @@
 4 5 6 
 7 8 9 
 ootpt =>1 2 3 6 9 8 7 4 5
-*/\
-#include<stdio.h>
+*/
+#include<stdio.h> 
 int main(){
     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int top=0,right=2,bottom=2,left=0;
-    while(top<bottom && left<right){
-    for(int i=0;i<3;i++){
-        printf("%d ",arr[top][i]);
+    int top=0,bottom=2;
+    int left=0,right=2;
+    while(top<=bottom && left<=right){
+        for(int i=left;i<=right;i++){
+            printf("%d ",arr[top][i]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++){
+            printf("%d ",arr[i][right]);
+        }
+        right--;
+        for (int i=right;i>=left;i--) {
+            printf("%d ", arr[bottom][i]);
+        }
+        bottom--;
+        for (int i=bottom;i>=top;i--){
+            printf("%d ", arr[i][left]);
+        }
+        left++;
     }
-    top++;
-    for(int i=top;i<=bottom;i++){
-        printf("%d ",arr[i][right]);
-    }
-    right--;
-    for (int i=right;i>=left;i--) {
-                printf("%d ", arr[bottom][i]);
-            }
-    bottom--;
-    for (int i=bottom;i>=top;i--){
-        printf("%d ", arr[i][left]);
-    }
-    left++;
-    for(int i=left;i<=right;i++){
-        printf("%d ", arr[right][i]);
-    }
-}
 }
