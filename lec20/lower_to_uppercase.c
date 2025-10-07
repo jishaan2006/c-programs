955164e150b46c98146d0f284d38298d291e9b84
Jishaan Khatri <jishaan123465@gmail.com>
2025-10-07 23:17:12 +0530

    String

---
+++ lec20/lower_to_uppercase.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    printf("Enter a sring in lower case :");
+    scanf("%s",str);
+    int length=strlen(str);
+    for (int i = 0; i < length; i++)
+    {
+        str[i]=str[i]-32;
+    }
+    for (int i = 0; i < length; i++){
+        printf("%c",str[i]);
+    }
+}
