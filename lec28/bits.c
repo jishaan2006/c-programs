979e584ef5bdd88e797a6c9ec9722a27cd3889db
Jishaan Khatri <jishaan123465@gmail.com>
2025-10-28 22:50:40 +0530

    Bits & Pallindrome String

---
+++ lec28/bits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter nummber :\n");
+    scanf("%d",&n);
+    if(n&1){
+        printf("Odd");
+    }
+    else{
+        printf("Even");
+    }
+    return 0;
+}
